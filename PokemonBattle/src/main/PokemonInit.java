55a58dce332c86f0a9db64e13babd829451497c7
Jeniza Chala <52945554+jenizachala@users.noreply.github.com>
2022-04-21 23:58:25 -0500

    Add files via upload

--- PokemonBattle/src/main/PokemonInit.java
+++ PokemonBattle/src/main/PokemonInit.java
@@ -71,3 +71,31 @@ public class PokemonInit extends JFrame implements ActionListener {
 
   }
 }
+
+//package main;
+//
+//import pokemon.OpenNotifyPokemonWebService;
+//import pokemon.PokemonBattle;
+//
+//import java.util.Locale;
+//import java.util.stream.Stream;
+//
+//public class PokemonInit {
+//
+//  public static void main(String[] args) {
+//
+//    PokemonBattle pokemonWebService = new PokemonBattle();
+//    pokemonWebService.setWebService(new OpenNotifyPokemonWebService());
+//
+//    String[] listOfPokemon = Stream.of(args)
+//        .flatMap(a -> Stream.of(a.split(",")))
+//        .toArray(String[]::new);
+//
+//    for (String pokemon : listOfPokemon) {
+//      System.out.println(pokemon + " receives double damage from: " +
+//          pokemonWebService.retrieveWeaknessesOfPokemon(
+//              pokemon.toLowerCase(Locale.ROOT)));
+//    }
+//
+//  }
+//}
